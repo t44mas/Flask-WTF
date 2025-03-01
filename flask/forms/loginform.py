43d4d246eaae43faebf73268c40cc9b40b342307
Kenti <matniyazov@inbox.ru>
2025-03-01 20:06:44 +0400

    fifth ex

--- flask/forms/loginform.py
+++ flask/forms/loginform.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.fields.numeric import DecimalField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
+    id_astronaut = DecimalField('id астронавта', validators=[DataRequired()])
+    password_astronaut = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    id_captain = DecimalField('id капитана', validators=[DataRequired()])
+    password_captain = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
