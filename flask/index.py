f3e4027da2f5dfa9d6099837d87bb10eb893f0a1
t44mas <motinartem08@gmail.com>
2025-03-08 14:50:38 +0400

    home - sec

--- flask/index.py
+++ flask/index.py
@@ -5,12 +5,15 @@ from forms.loginform import LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+
 @app.route('/')
 @app.route('/index/<title>')
 def index(title):
     title = title
     user = "Ученик Яндекс.Лицея"
     return render_template('index.html', title=title)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -18,14 +21,17 @@ def login():
         return redirect('/index')
     return render_template('loginform.html', title='Авторизация', form=form)
 
+
 @app.route('/training/<prof>')
 def training(prof):
     return render_template('training.html', prof=prof)
+
 
 @app.route('/list_prof/<type_list>')
 def list_prof(type_list):
     prof = ['врач', 'инженер', 'биолог', 'строитель']
     return render_template('list_prof.html', type=type_list, prof=prof)
+
 
 @app.route('/answer')
 @app.route('/auto_answer')
@@ -40,6 +46,8 @@ def answer():
     params['motivation'] = 'Всегда мечтал застрять на Марсе!'
     params['ready'] = True
     return render_template('auto_answer.html', **params)
+
+
 @app.route('/distribution')
 def distribution():
     astronauts = [
@@ -55,5 +63,14 @@ def distribution():
         "Тим Пик",
     ]
     return render_template('distribution.html', list1=astronauts)
+
+
+@app.route('/table/<gender>/<age>')
+def table(gender, age):
+    gender = gender
+    age = int(age)
+    return render_template('table.html', gender=gender, age=age)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
